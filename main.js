2591cae14cd7998f7fe546c23e3c4d4a343e0e71
Koralai <carolyn.anne.clark@gmail.com>
2017-09-19 18:48:00 -0500

    Made quotes an array, added random color generation but button/border colors are not matching

--- main.js
+++ main.js
@@ -1,58 +1,39 @@
 $(function(){
 
 var quotes = [
-    {
-        "id": 1,
-        "text": "I never saw anybody take so long to dress, and with such little result."
-    },
-    {
-        "id": 2,
-        "text": "It is what you read when you don't have to that determines what you will be when you can't help it."
-    },
-    {
-        "id": 3,
-        "text": "I don't want to go to heaven. None of my friends are there."
-    },
-    {
-        "id": 4,
-        "text": "Every saint has a past, and every sinner has a future."
-    },
-    {
-        "id": 5,
-        "text": "I am not young enough to know everything."
-    },
-    {
-        "id": 6,
-        "text": "Man is least himself when he talks in his own person. Give him a mask, and he will tell you the truth."
-    },
-    {
-        "id": 7,
-        "text": "I can resist anything except temptation."
-    },
-    {
-        "id": 8,
-        "text": "Quotation is a serviceable substitute for wit."
-    },
-    {
-        "id": 9,
-        "text": "Fashion is a form of ugliness so intolerable that we have to alter it every six months."
-    },
-    {
-        "id": 10,
-        "text": "Some cause happiness wherever they go; others whenever they go."
-    },
+    "I never saw anybody take so long to dress, and with such little result.",
+    "It is what you read when you don't have to that determines what you will be when you can't help it.",
+    "I don't want to go to heaven. None of my friends are there.",
+    "Every saint has a past, and every sinner has a future.",
+    "I am not young enough to know everything.",
+    "Man is least himself when he talks in his own person. Give him a mask, and he will tell you the truth.",
+    "I can resist anything except temptation.",
+    "Quotation is a serviceable substitute for wit.",
+    "Fashion is a form of ugliness so intolerable that we have to alter it every six months.",
+    "Some cause happiness wherever they go; others whenever they go.",
 ];
 
-function getRandomIntInclusive(min, max) {
+function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive 
   }
 
-$("#quote-text").html(quotes[getRandomIntInclusive(0,9)]["text"]);
+function getRandomColor() {
+    var letters = '789ABCD';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 6)];
+    }
+    return color;
+  }
+
+$("#quote-text").html(quotes[getRandomInt(0,quotes.length)]);
 
 $("#btn-get").click(function(){
-    $("#quote-text").html(quotes[getRandomIntInclusive(0,9)]["text"]); //Gets the "text" value of the object at a random index of the array
+    $("#quote-text").html(quotes[getRandomInt(0,quotes.length)]); //Gets the "text" value of the object at a random index of the array
+    $(".quote").css("border-color", getRandomColor());
+    $("button").css("background", getRandomColor());
 });
 
 });
