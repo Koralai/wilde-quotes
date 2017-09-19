29cd5a439d1857103f53bb205ac8e9a574f045a4
Koralai <carolyn.anne.clark@gmail.com>
2017-09-19 07:11:31 -0500

    Installed jquery, added  random quote functionality on button click and page load

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+$(function(){
+
 var quotes = [
     {
         "id": 1,
@@ -41,10 +43,16 @@ var quotes = [
     },
 ];
 
-function getRandomInt(min, max) {
+function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; 
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
   }
 
-console.log(quotes[getRandomInt(0,9)]["text"]);
+$("#quote-text").html(quotes[getRandomIntInclusive(0,9)]["text"]);
+
+$("#btn-get").click(function(){
+    $("#quote-text").html(quotes[getRandomIntInclusive(0,9)]["text"]); //Gets the "text" value of the object at a random index of the array
+});
+
+});
